c346207625d79ea010e10f9d1380d1a46bfd64d5
coding-cole <53341252+coding-cole@users.noreply.github.com>
2019-12-02 17:22:10 +0100

    include AppDatabase class

--- app/src/main/java/com/coding_cole/tasktimer/AppDatabase.java
+++ app/src/main/java/com/coding_cole/tasktimer/AppDatabase.java
@@ -1,4 +1,48 @@
 package com.coding_cole.tasktimer;
 
-class AppDatabase {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+/**
+ * basic database class for the app
+ *
+ * the only class that should use this is the AppProvider
+ */
+
+class AppDatabase extends SQLiteOpenHelper {
+    private static final String TAG = "AppDatabase";
+
+    public static final String DATABASE_NAME = "TaskTmer.db";
+    public static final int DATABASE_VERSION = 1;
+
+    private AppDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d(TAG, "AppDatabase: constructor");
+    }
+
+    /**
+     *
+     * Get an instance of the apps singleton database helper object
+     * @param context the content providers context
+     * @return a sqlite database object
+     */
+
+    static AppDatabase getInstance(Context context) {
+
+        return null;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/com/coding_cole/tasktimer/TasksContract.java
+++ app/src/main/java/com/coding_cole/tasktimer/TasksContract.java
@@ -1,4 +1,20 @@
 package com.coding_cole.tasktimer;
 
+import android.provider.BaseColumns;
+
 public class TasksContract {
+
+    static final String TABLE_NAME = "TAsks";
+
+    // tasks fields
+    public static class Coloumns {
+        public static final String _ID = BaseColumns._ID;
+        public static final String TASKS_NAME = "Name";
+        public static final String TASKS_DESCRIPTION = "Dscription";
+        public static final String TASKS_SORTORDER = "SortOrder";
+
+        private Coloumns() {
+            // private constructor to prevent instantiation
+        }
+    }
 }
